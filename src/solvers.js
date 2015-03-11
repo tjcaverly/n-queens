254a7be3702389fb94ad7e14ca040a4af5603585
MKS-Student <mks.student@hackreactor.com>
2015-03-11 16:00:03 -0700

    Solution for findNRooks and countNRooks

--- src/solvers.js
+++ src/solvers.js
@@ -14,18 +14,57 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n:n});
+  var foundSolution = false;
 
+  var placeRookN = function(row) {
+
+
+    for (var i=0; i<n; i++) {
+      solution.togglePiece(row, i);
+      if (!solution.hasAnyColConflicts()){
+        if (row === n-1){
+          foundSolution = true;
+          return 
+        }
+        placeRookN(row + 1);
+      }
+      if (foundSolution) {
+        return;
+      }
+      solution.togglePiece(row, i);
+    }
+  }
+
+  placeRookN(0);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solution = new Board({n:n});
+  var solutionCount = 0;
 
+  var placeRookN = function(row) {
+
+
+    for (var i=0; i<n; i++) {
+      solution.togglePiece(row, i);
+      if (!solution.hasAnyColConflicts()){
+        if (row === n-1){
+          solutionCount++;
+          solution.togglePiece(row, i);
+          return;
+        }
+        placeRookN(row + 1);
+      }
+      solution.togglePiece(row, i);
+    }
+  }
+  placeRookN(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };

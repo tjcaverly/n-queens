52c11b65c4a92214812a2201f316ba0178b507a9
MKS-Student <mks.student@hackreactor.com>
2015-03-11 16:38:56 -0700

    Completed findNQueens and countNQueens

--- src/solvers.js
+++ src/solvers.js
@@ -59,7 +59,7 @@ window.countNRooksSolutions = function(n) {
         solution.togglePiece(row, index);
         return;
         }
-        
+
       placeRookN(row + 1, possibleColumns.slice(0,i).concat(possibleColumns.slice(i+1)));
       
       solution.togglePiece(row, index);
@@ -74,17 +74,57 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n:n}); //fixme
+  var foundSolution = false;
 
+  var placeQueenN = function(row, possibleColumns){
+    for (var i = 0; i < possibleColumns.length; i++){
+      var index = possibleColumns[i];
+      solution.togglePiece(row, index);
+      if (!solution.hasMajorDiagonalConflictAt(index - row) &&
+          !solution.hasMinorDiagonalConflictAt(index + row)){
+        if (row === n - 1){
+          foundSolution = true;
+          return;
+        }
+        placeQueenN(row + 1, possibleColumns.slice(0,i).concat(possibleColumns.slice(i + 1)));
+      }
+      if (foundSolution)
+        return;
+      solution.togglePiece(row, index);
+    }
+  }
+  placeQueenN(0, _.range(n));
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solution = new Board({n:n}); //fixme
+  var solutionCount = 0;
 
+  if (n === 0)
+    return 1;
+
+  var placeQueenN = function(row, possibleColumns){
+    for (var i = 0; i < possibleColumns.length; i++){
+      var index = possibleColumns[i];
+      solution.togglePiece(row, index);
+      if (!solution.hasMajorDiagonalConflictAt(index - row) &&
+          !solution.hasMinorDiagonalConflictAt(index + row)){
+        if (row === n - 1){
+          solutionCount++;
+          solution.togglePiece(row, index);
+          return;
+        }
+        placeQueenN(row + 1, possibleColumns.slice(0,i).concat(possibleColumns.slice(i + 1)));
+      }
+      solution.togglePiece(row, index);
+    }
+  }
+  placeQueenN(0, _.range(n));
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
